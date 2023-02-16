a24bfad9828ef36cf03e145014d4420b7711311b
iulianivg <ivighenie@yahoo.com>
2023-02-17 00:40:15 +0400

    first push

---
+++ src/graph.js
@@ -0,0 +1,47 @@
+import fetch from 'cross-fetch';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+
+import { ARBITRUM, AVALANCHE, POLYGON } from './addresses'
+
+const apolloOptions = {
+  query: {
+    fetchPolicy: 'no-cache'
+  },
+  watchQuery: {
+    fetchPolicy: 'no-cache'
+  }
+}
+
+const arbitrumPricesClient = new ApolloClient({
+  link: new HttpLink({ uri: 'https://api.thegraph.com/subgraphs/name/gmx-io/gmx-arbitrum-prices', fetch }),
+  cache: new InMemoryCache(),
+  defaultOptions: apolloOptions
+})
+
+const avalanchePricesClient = new ApolloClient({
+  link: new HttpLink({ uri: 'https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-prices', fetch }),
+  cache: new InMemoryCache(),
+  defaultOptions: apolloOptions
+})
+
+const polygonPricesClient = new ApolloClient({
+  link: new HttpLink({ uri: 'https://api.thegraph.com/subgraphs/name/gmx-io/gmx-avalanche-prices', fetch }),
+  cache: new InMemoryCache(),
+  defaultOptions: apolloOptions
+})
+
+function getPricesClient(chainId) {
+  if (chainId === ARBITRUM) {
+    return arbitrumPricesClient
+  } else if (chainId === AVALANCHE) {
+    return avalanchePricesClient
+  } else if (chainId === POLYGON) {
+    return polygonPricesClient
+  } else {
+    throw new Error(`Invalid chainId ${chainId}`)
+  }
+}
+
+module.exports = {
+  getPricesClient
+}
