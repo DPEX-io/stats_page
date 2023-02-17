c046b8b804944257633444db7a21d0f28d4f0029
iulianivg <ivighenie@yahoo.com>
2023-02-18 02:05:57 +0400

    fix seo

--- src/routes.js
+++ src/routes.js
@@ -115,7 +115,7 @@ export default function routes(app) {
           <head>
               <meta http-equiv="X-UA-Compatible" content="IE=edge" />
               <meta charset="utf-8" />
-              <title>AMX analytics</title>
+              <title>DPEX analytics</title>
               <meta name="viewport" content="width=device-width, initial-scale=1">
               <link rel="icon" type="image/png" href="/favicon.png" />
               ${cssAssetsTag}
